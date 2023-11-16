e30ed68719e6d60716712ad19037768c0617696c
nehuen <nenugonz@gmail.com>
2023-11-16 16:46:26 -0300

    funciona?

--- IntArtVerbos/src/main/java/com/example/IntArtVerbos/entity/word.java
+++ IntArtVerbos/src/main/java/com/example/IntArtVerbos/entity/word.java
@@ -1,8 +1,20 @@
 package com.example.IntArtVerbos.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class word {
+private static final GenerationType GenerationType = null;
     
-    private String word;
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    @Column(unique = true,nullable =false)
+     private String word;
 
     public String getWord() {
         return word;
