a63c8fb2fc8f361adbde59c93b391e6ee3344bf0
nehuen <nenugonz@gmail.com>
2023-11-16 13:54:20 -0300

    mensaje

---
+++ IntArtVerbos/src/main/java/com/example/IntArtVerbos/IntArtVerbosApplication.java
@@ -0,0 +1,13 @@
+package com.example.IntArtVerbos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntArtVerbosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntArtVerbosApplication.class, args);
+	}
+
+}
---
+++ IntArtVerbos/src/test/java/com/example/IntArtVerbos/IntArtVerbosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.IntArtVerbos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntArtVerbosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
